cb95dcf294c33d0adf56b48c74d7d2279942ba36
pTinosq <tinosps@gmail.com>
2025-07-20 17:25:03 +0100

    Decided to include urls.txt by default

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import requests
 from datetime import datetime
 import os
 
+URLS_TXT_PATH = "./urls.txt"
+
 
 def download_fragmented_mp4(url):
     """Download a full fragmented.mp4 stream to a local file."""
     headers = {"Range": "bytes=0-", "Referer": "https://cardiff.cloud.panopto.eu/"}
 
-    filename = f"lecture_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
+    os.makedirs("lectures", exist_ok=True)
+    filename = os.path.join(
+        "lectures", f"lecture_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
+    )
     print(f"Downloading to: {filename}")
 
     with requests.get(url, headers=headers, stream=True) as r:
@@ -31,8 +36,12 @@ def download_from_txt(file_path):
 
 
 if __name__ == "__main__":
-    path = input("Path to TXT file containing Panopto URLs: ").strip()
-    if os.path.isfile(path):
-        download_from_txt(path)
+    # Check if urls.txt exists already (and use as default)
+    if os.path.exists(URLS_TXT_PATH):
+        print("✅ Found urls.txt file")
+        download_from_txt(URLS_TXT_PATH)
+
     else:
-        print("❌ File not found.")
+        print(
+            "❌ urls.txt file not found. Please create this file next to the `main.py` file"
+        )
