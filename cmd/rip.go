19a04c0c9f932b0474d23390a0d8f959b143d3d1
pTinosq <tinosps@gmail.com>
2025-07-20 18:10:50 +0100

    Add cobra and rip command

--- cmd/rip.go
+++ cmd/rip.go
@@ -1,40 +1,55 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
+	"os"
+
+	"net/url"
 
 	"github.com/spf13/cobra"
 )
 
+var batch string
+var single string
+
+func isValidURL(u string) bool {
+	_, err := url.ParseRequestURI(u)
+	return err == nil
+}
+
 // ripCmd represents the rip command
 var ripCmd = &cobra.Command{
 	Use:   "rip",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "The rip command downloads full-length Panopto recordings based on a recording URL",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if single != "" {
+			if !isValidURL(single) {
+				return fmt.Errorf("invalid URL: %s", single)
+			}
+			// process single URL...
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rip called")
+		if batch != "" {
+			if _, err := os.Stat(batch); err != nil {
+				return fmt.Errorf("cannot read file: %s", batch)
+			}
+			// process batch file...
+		}
+
+		return nil
+	},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if (single == "" && batch == "") || (single != "" && batch != "") {
+			cmd.Help()
+			return fmt.Errorf("you must specify exactly one of --single or --batch")
+		}
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(ripCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ripCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// ripCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	ripCmd.Flags().StringVarP(&batch, "batch", "b", "", "Path to a file containing URLs to download")
+	ripCmd.Flags().StringVarP(&single, "single", "s", "", "A single Panopto video URL to download")
 }
